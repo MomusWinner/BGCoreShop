3efe16ce7d228a5d89195545de736caa0cfef07d
Ilya Polovinkin <some@mail.com>
2022-10-01 14:28:22 +0300

    Implemented test scene

--- Game/LocationObjects/Characters/Model/BaseCharacterSetting.cs
+++ Game/LocationObjects/Characters/Model/BaseCharacterSetting.cs
@@ -5,16 +5,5 @@ namespace Game.Characters.Model
 {
     public abstract class BaseCharacterSetting : ViewSetting
     {
-        public float Speed => speed;
-        public float Gravity => gravity;
-        public float JumpHeight => jumpHeight;
-        public float GroundDistance => groundDistance;
-        public LayerMask GroundMask => groundMask;
-        
-        [SerializeField] private float speed = 12f;
-        [SerializeField] private float gravity = -10f;
-        [SerializeField] private float jumpHeight = 2f;
-        [SerializeField] private float groundDistance = 0.4f;
-        [SerializeField] private LayerMask groundMask;
     }
 }
