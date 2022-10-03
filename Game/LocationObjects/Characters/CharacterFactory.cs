3d95d506897955c9af72377dd053c41e0e225e35
ilyapolovinkin <2190055@gmail.com>
2022-10-04 01:46:47 +0300

    Update character factory

--- Game/LocationObjects/Characters/CharacterFactory.cs
+++ Game/LocationObjects/Characters/CharacterFactory.cs
@@ -2,6 +2,7 @@ using Core.Locations.Model;
 using Game.Characters.Model;
 using Game.GameData;
 using GameLogic;
+using GameLogic.Npcs;
 
 namespace Game.Characters
 {
@@ -10,10 +11,13 @@ namespace Game.Characters
         public static ICharacter CreateCharacter(Location parentLocation, CharacterType characterType,
             BaseCharacterSetting setting, IContext context)
         {
-            if (setting is PlayerSetting playerSetting &&
-                characterType is CharacterType.Player )
+            if (setting is PlayerSetting playerSetting && characterType is CharacterType.Player )
             {
                 return new Player(parentLocation, "[Player]", playerSetting, context);
+            }
+            if (setting is NpcSetting npcSetting && characterType is CharacterType.Npc )
+            {
+                return new Npc(parentLocation, "[NPC]", npcSetting, context);
             }
 
             return null;
