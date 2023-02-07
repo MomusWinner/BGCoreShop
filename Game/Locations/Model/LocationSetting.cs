3e3fd28d182c172ccbb1f79deee8cfa598099bb1
Ilya Polovinkin <some@mail.com>
2023-02-07 15:25:53 +0300

    Fix section drop

--- Game/Locations/Model/LocationSetting.cs
+++ Game/Locations/Model/LocationSetting.cs
@@ -1,9 +1,16 @@
+using System.Linq;
 using Game.Settings;
 
 namespace Core.Locations.Model
 {
     public abstract class LocationSetting : ViewSetting
     {
+        public BaseSetting[] childSettings;
+
+        public T GetConfig<T>() where T: BaseSetting
+        {
+            return childSettings.FirstOrDefault(s => s is T) as T;
+        }
         public BaseSetting[] locationObjectsSettings;
     }
 }
