ead9bfa772177ccc5221a6716f535cd3101cfe5f
Ilya Polovinkin <some@mail.com>
2023-02-13 17:10:37 +0300

    gyro implemented

--- Core/Bootstrap.cs
+++ Core/Bootstrap.cs
@@ -9,7 +9,6 @@ namespace Core.Main
         {
             GEvent.Attach(GlobalEvents.Start, OnStart);
             GEvent.Attach(GlobalEvents.Restart, OnRestart);
-            Application.targetFrameRate = 30;
         }
 
         private static void OnStart(object _)
