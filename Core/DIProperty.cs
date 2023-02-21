1a57081c97f05fc729a56ea610e8ed38e8d2f636
Ilya Polovinkin <some@mail.com>
2023-02-21 09:00:26 +0300

    Added DIProperty.cs

---
+++ Core/DIProperty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BGCore.Core
+{
+    public class DIProperty<T>
+    {
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                CallAllActions();
+            }
+        }
+
+        private T value;
+        private Dictionary<Action<T>, Action<T>> actions;
+        
+        public DIProperty()
+        {
+            actions = new Dictionary<Action<T>, Action<T>>();
+        }
+
+        public void Subscribe(Action<T> action)
+        {
+            if (!actions.ContainsKey(action))
+                actions.Add(action, action);
+        }
+
+        public void Unsubscribe(Action<T> action)
+        {
+            if (actions.ContainsKey(action))
+                actions.Remove(action);
+        }
+
+        public void UnsubscribeAll()
+        {
+            actions.Clear();
+        }
+
+        private void CallAllActions()
+        {
+            foreach (var action in actions)
+            {
+                action.Value.Invoke(value);
+            }
+        }
+    }
+}
